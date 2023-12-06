bf0dc8027f3d913bc46f994406fbce0d1fcd527f
Victor Lopes <121266927+vlopess@users.noreply.github.com>
2023-12-06 20:05:58 -0300

    Update SudokuMain.java

--- src/sudoku/SudokuMain.java
+++ src/sudoku/SudokuMain.java
@@ -120,17 +120,17 @@ public class SudokuMain extends JFrame {
         		Countdown.getInstance().startTimer();
         		if (comboBox.getSelectedItem().equals("Fácil")) {
 				board.newGame(Levels.EASY);
-				Error.getInstance().setLevel(Levels.EASY);
+				//Error.getInstance().setLevel(Levels.EASY);
 			}
 
         		if (comboBox.getSelectedItem().equals("Médio")) {
 				board.newGame(Levels.MEDIUM);
-				Error.getInstance().setLevel(Levels.MEDIUM);
+				//Error.getInstance().setLevel(Levels.MEDIUM);
 			}
         			
         		if (comboBox.getSelectedItem().equals("Difícil")) {
 				board.newGame(Levels.HARD);
-				Error.getInstance().setLevel(Levels.HARD);
+				//Error.getInstance().setLevel(Levels.HARD);
 			}
         			
         		label.setText("Nível: " + (String) comboBox.getSelectedItem());
